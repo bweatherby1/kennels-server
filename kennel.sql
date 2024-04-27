66a39e5e575a4d46b30a8f9712381ad380dbbef8
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-04-27 11:37:05 -0500

    added delete to all

--- kennel.sql
+++ kennel.sql
@@ -42,6 +42,7 @@ INSERT INTO `Employee` VALUES (null, "Kristen Norris", "100 Main St", 1);
 INSERT INTO `Employee` VALUES (null, "Meg Ducharme", "404 Unknown Ct", 2);
 INSERT INTO `Employee` VALUES (null, "Hannah Hall", "204 Empty Ave", 1);
 INSERT INTO `Employee` VALUES (null, "Leah Hoefling", "200 Success Way", 2);
+INSERT INTO `Employee` VALUES (null, "Kristen Norris", "100 Main St", 1);
 
 
 INSERT INTO `Customer` VALUES (null, "Mo Silvera", "201 Created St", "mo@silvera.com", "password");
@@ -59,6 +60,7 @@ INSERT INTO `Animal` VALUES (null, "Daps", "Kennel", "Boxer", 2, 2);
 INSERT INTO `Animal` VALUES (null, "Cleo", "Kennel", "Poodle", 2, 2);
 INSERT INTO `Animal` VALUES (null, "Popcorn", "Kennel", "Beagle", 3, 2);
 INSERT INTO `Animal` VALUES (null, "Curly", "Treatment", "Poodle", 4, 2);
+INSERT INTO `Animal` VALUES (null, "Daps", "Kennel", "Boxer", 2, 2);
 
 -- Get only the animal rows where the `id` field value is 3
 SELECT
